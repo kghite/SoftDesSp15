d091500b430d84ffb28c02312f64cf0f5308af6d
Dennis Chen <dennis.chen@students.olin.edu>
2015-02-26 14:14:15 -0500

    comp art grade comments

--- computational_art/recursive_art.py
+++ computational_art/recursive_art.py
@@ -20,6 +20,8 @@ def build_random_function(min_depth, max_depth):
     function_parts = ["prod", "avg", "cos_pi", "sin_pi", "divide", "power"]
     function = random.choice(function_parts)
 
+    #it's often kind of a convention (at least from what I've seen) to put your base case in a recursive function first, instead of at the end.
+
     if max_depth > 0:
         if min_depth > 0:
             if function == "avg" or function == "prod":
@@ -38,7 +40,11 @@ def build_random_function(min_depth, max_depth):
                     return "y"
             else:
                 return [function, build_random_function(min_depth, max_depth-1)]
+                #looks like you forgot to give min_depth - 1 as the argument here. Or was this intentional? 
+                #Correct me if I'm wrong, it looks like your approach to randomly choosing whether or not to go deeper/recurse more once you've reached min_depth is to randomly choose a function, and then go deeper if that function happens to be cos_pi, sin_pi, divide, or power. This works, but isn't the most robust, as it means that you can't have prod or avg appear deeper than min_depth in your function, reducting the randomness.
+                #that could all be arguably intentional, but there would have to be comments explaining your thought process.
     else:
+        #or, try return random.choice(['x','y'])!
         if random.randint(0,1) == 1:
             return "x"
         else:
@@ -78,6 +84,7 @@ def evaluate_random_function(f, x, y):
         return y
     else:
         raise Exception('Could not evalutate function ' + str(f))
+    #nice and elegant. :)
 
 
 def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
@@ -108,6 +115,8 @@ def remap_interval(val, input_interval_start, input_interval_end, output_interva
     b = float(input_interval_end)
     c = float(output_interval_start)
     d = float(output_interval_end)
+
+    #good thought to force everything to be a float, but do note that python will cast ints to float automatically whenever you have a float involved in the operation. So, all you need to guard against is the division being integer division if you get really unlucky. To get rid of that problem, multiply by 1.0 first. So, this code segment could be reduced to term1 = 1.0*(val - input_interval_start)/(input_interval_end - input_interval_start)
 
     term1 = (x - a)/(b - a)
     term2 = d - c
